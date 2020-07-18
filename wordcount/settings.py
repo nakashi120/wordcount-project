53c26c7e1efc0437f14436cc3f6eccf7ee7529aa
Satoshi Nakashima <nakashi120@gmail.com>
2020-07-18 22:34:27 +0900

    CHANGE SECRET_KEY

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -12,15 +12,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Setting SECRETKEY
+try:
+    from .local_setting import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1926mc=r(maz(zzvx4!&p9!aq*)je%y&el2ppg!4yo2we0$0@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
